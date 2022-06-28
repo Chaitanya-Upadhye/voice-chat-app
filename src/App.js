16028d0ea232cccff7ab9771a2bb2b31595a9c6c
Chaitanya <chaitu447@gmail.com>
2022-06-28 21:22:02 +0530

    changed browser router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import { Box } from "@chakra-ui/react";
 import { Switch } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Route, Router } from "react-router-dom";
-import Login from "./components/Login";
 import RoomsList from "./components/Rooms/RoomsList";
-import AuthorizedRoute from "./components/Routes/AuthorizedRoute";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" component={RoomsList} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
